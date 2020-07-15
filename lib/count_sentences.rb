f64dadd84bd5808d683639a5ba5ec9cbb7c02a96
Andre Cunningham <andrecunningham62@gmail.com>
2020-07-15 00:56:47 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -6,11 +6,7 @@ def sentence?
   test_string =  string.new("HI!")
   test_string.sentence?
 end
-  Lets say we created a test_string, and called that method sentence? on it. How do we refer to test_string inside the method?
-User avatar
-John Chriest 6 HOURS AGO
-test_string = String.new("yippeee")
-test_string.sentence?
+  
      attr_accessor :name
  
   def initialize(name)
