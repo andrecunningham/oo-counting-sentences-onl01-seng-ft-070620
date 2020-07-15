724d0a51cb01b81864703233818eac8b1956b308
Andre Cunningham <andrecunningham62@gmail.com>
2020-07-15 00:47:09 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,8 +2,16 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+     attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+    def say hi
+      "HI!"
+    end
+   
   end
 
   def question?
